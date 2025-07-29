2c174224482c700c0a0771bafdc58b7ea5689efe
wfcornelissen <wfcornelissen@gmail.com>
2025-07-29 18:16:29 +0200

    C2L1 in prog

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileserver))
+	mux.Handle("/assets/logo.png", fileserver)
+	mux.HandleFunc("/healthz", readyEndpoint)
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	fmt.Println("Starting serveron port 8080")
+	fmt.Println("Starting server on port 8080")
 	server.ListenAndServe()
+
+}
+
+func readyEndpoint(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(http.StatusText(http.StatusOK)))
 }
